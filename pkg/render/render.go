7a0fa88d59f95ae7934eb59a20315f4a6c1293fc
sarathy <sarathylanz@gmail.com>
2023-08-21 12:49:12 +0530

    template cache complex mechanism

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,54 +1,72 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
 	"log"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
-func RenderTemplate_old(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl, "./templates/base.layout.tmpl")
-	err := parsedTemplate.Execute(w,nil)
-	if err != nil {
-		fmt.Fprintf(w, "Error executing template")
-	}
-}
-
-var tc = make(map[string]*template.Template)
-
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	log.Println("into render  func")
-	var existingTemplate *template.Template
-	var err error
 
-	_, inMap := tc[tmpl]
-	log.Println(inMap)
-	if !inMap {
-		log.Println("creating Cache template")
-		err = createTemplateCache(tmpl)
-	} else {
-		log.Println("using Cache template")
+	// populate the template and cache them
+	templates,err:=createCacheTEmplate()
+	if err != nil {
+		log.Fatal(err)
 	}
+	
+	// get the cache template
+	currentTemplate,ok := templates[tmpl]
 
-	existingTemplate = tc[tmpl]
-	err = existingTemplate.Execute(w,nil)
+	if !ok {
+		log.Fatal(err)
+	}
+	// render the template
+
+	buf:=new(bytes.Buffer)
+	err=currentTemplate.Execute(buf,nil)
+
 	if err != nil { 
-		log.Println(err)
+		log.Fatal(err)
 	}
 
+	_,err = buf.WriteTo(w)
+	if err != nil { 
+		log.Fatal(err)
+	}
 }
 
-func createTemplateCache(tmpl string) error {
-	templates := []string{
-		fmt.Sprintf("./templates/%s",tmpl),
-		"./templates/base.layout.tmpl",
+func createCacheTEmplate()(map[string]*template.Template,error) {
+	var ts *template.Template
+	var myTemplateCache = map[string]*template.Template{}
+
+	filenames,err:=filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil { 
+		return myTemplateCache,err
 	}
 
-	t,err := template.ParseFiles(templates...)
-	if err != nil { 
-		return err
+	for _,v:=range filenames {
+		templateName := filepath.Base(v)
+		ts,err = template.New(templateName).ParseFiles(v)
+		if err != nil { 
+			return myTemplateCache,err
+		}
+		layouts,err:=filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil { 
+			return myTemplateCache,err
+		}
+		if len(layouts) > 0 {
+			ts, err= ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil { 
+				return myTemplateCache,err
+			}
+		}
+		myTemplateCache[templateName] = ts
 	}
-	tc[tmpl] = t
-	return nil
+
+	return myTemplateCache,nil
 }
+
+
